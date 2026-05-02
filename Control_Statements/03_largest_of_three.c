ee34d1afee643dc6345c8a149bdffb891166b8c8
Narsing Narvate <narvatenarsing8@gmail.com>
2026-05-02 21:03:36 +0530

    Create 03_largest_of_three.c
    Add program to find largest of three numbers

---
+++ Control_Statements/03_largest_of_three.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a >= b && a >= c)
+        printf("A is largest");
+    else if(b >= a && b >= c)
+        printf("B is largest");
+    else
+        printf("C is largest");
+
+    return 0;
+}
+Commit:
