0dc547efcb30a34a63d64e08c6544cc306513709
Narsing Narvate <narvatenarsing8@gmail.com>
2026-05-06 22:22:43 +0530

    Create 06_even_odd_switch.c
    Add even or odd program using switch case

---
+++ Control_Statements/06_even_odd_switch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    switch(num % 2) {
+        case 0: printf("Even"); break;
+        case 1: printf("Odd"); break;
+    }
+
+    return 0;
+}
